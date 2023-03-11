18c59c4b55ab9008f1fa21036b3ba6fd2ef2f24b
Jon Burns <jcb42891@gmail.com>
2023-03-11 14:33:31 -0500

    moved meals code into component

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -2,20 +2,6 @@ import React, { useState, useEffect } from "react";
 import AppRouter from "./AppRouter";
 
 const App = () => {
-  let [meals, setMeals] = useState(null);
-
-  useEffect(() => {
-    fetch("/api/meals")
-      .then((response) => response.json())
-      .then((data) => setMeals(data));
-  }, []);
-
-  return (
-    <AppRouter />
-    // <div>
-    //   <h3>Here are the meals we have so far:</h3>
-    //   <ul>{meals && meals.map((m) => <li>{m.name}</li>)}</ul>
-    // </div>
-  );
+  return <AppRouter />;
 };
 export default App;
--- app/javascript/components/Meals.jsx
+++ app/javascript/components/Meals.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const Meals = () => {
-  return <h1>Welcome to the meal list!</h1>;
+  let [meals, setMeals] = useState(null);
+
+  useEffect(() => {
+    fetch("/api/meals")
+      .then((response) => response.json())
+      .then((data) => setMeals(data));
+  }, []);
+
+  return (
+    <div>
+      <h3>Here are the meals we have so far:</h3>
+      <ul>{meals && meals.map((m) => <li>{m.name}</li>)}</ul>
+    </div>
+  );
 };
 
 export default Meals;
