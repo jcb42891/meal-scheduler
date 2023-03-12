d7fee43d66d13d1a06aaacca328f66f59b398828
Jon Burns <jcb42891@gmail.com>
2023-03-12 15:04:53 -0400

    ordered results for scheduled meals api

--- app/controllers/scheduled_meals_controller.rb
+++ app/controllers/scheduled_meals_controller.rb
@@ -3,13 +3,12 @@ class ScheduledMealsController < ApplicationController
     def get_all_for_anchor_date
         if params[:anchor_date]
             anchor_date = Date.parse(params[:anchor_date])
-            @scheduled_meals = ScheduledMeal.where("scheduled_date >= ? AND scheduled_date <= ?", anchor_date, anchor_date + 7.days)
+            @scheduled_meals = ScheduledMeal.where("scheduled_date >= ? AND scheduled_date <= ?", anchor_date, anchor_date + 7.days).order(:scheduled_date, :asc)
 
             res = []
             @scheduled_meals.each do |scheduled_meal|
                 res << { description: scheduled_meal.mealable.name, scheduled_date: scheduled_meal.scheduled_date}
             end
-            puts res
             render json: res
         else
             render json: { status: 400, message: "No date supplied."}
